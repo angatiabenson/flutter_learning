c8a64d7e28288468ee5e328e8a6cce3e2d40129c
angatiabenson <bensonetia@gmail.com>
2024-06-15 21:17:53 +0300

    feat: basic ui and using Image from assets and network

--- lib/screens/dasboard.dart
+++ lib/screens/dasboard.dart
@@ -11,8 +11,13 @@ class Dashboard extends StatelessWidget {
         title: Text("Dashboard".toUpperCase()),
         backgroundColor: Colors.blueGrey,
       ),
-      body: Center(
-        child: Text("Random value is ${getRandomNumber()}")
+      body: const Center(
+        child: Image(
+          image: NetworkImage("https://www.dairyfreestate.com/wp-content/uploads/2019/12/2019-word-growth-1024x587-1024x585.png"),
+          width: 200.0,
+          height: 200.0,
+          fit: BoxFit.contain,
+        )
       ),
     );
   }
